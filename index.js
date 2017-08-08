67b57cf0f6b3f46397d69a5ded97fa9a0e4d21aa
Dany Aguilar <dany.aguilar.c@gmail.com>
2017-08-08 01:02:42 -0500

    ahora si

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ if (!databaseUri) {
 var api = new ParseServer({
   databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: 'quitary',
-  masterKey: 'Theforce17', //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'quitary',
+  masterKey: process.env.MASTER_KEY || 'Theforce17', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
     classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
